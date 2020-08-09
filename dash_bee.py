01ce0876b95df050d3f74c337de4f5e42b793e68
kotternaj <jreddock@gmail.com>
2020-08-09 17:53:39 -0400

    created dropdown

--- dash_bee.py
+++ dash_bee.py
@@ -15,5 +15,25 @@ df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of
 df.reset_index(inplace=True)
 print(df[:5])
 
+app.layout = html.Div([
+    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+
+    dcc.Dropdown(id='slct_year',
+                 options=[
+                     {'label': '2015', 'value': 2015},
+                     {'label': '2016', 'value': 2016},
+                     {'label': '2017', 'value': 2017},
+                     {'label': '2018', 'value': 2018}],
+                multi=False,
+                value=2015,
+                style={'width': '40%'}
+                ),
+
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+    dcc.Graph(id='bee_map', figure={})
+])
+
+
  # if __name__ == '__main__':
  #     app.run_server(debug=True)
