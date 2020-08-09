a17c13a7692400d8db0ba1065267a0286e62c18d
kotternaj <jreddock@gmail.com>
2020-08-09 17:05:43 -0400

    created choropleth map using geojson

--- gamap.py
+++ gamap.py
@@ -1,15 +1,30 @@
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
 import pandas as pd
 import xlrd
 
-df = pd.read_excel('county.xlsx')
-df.rename(columns={'Unnamed: 2': 'county_code', 'Unnamed: 1': 'state_code',
-                'Unnamed: 6': 'area_name'}, inplace=True)
-ga_code = df['state_code'].str.contains('13')
-print(ga_code.count())
-# ga_county_codes = df[2] == ga_code
-# area_name = df[6]
-# print(df[us_county_codes, us_counties])
-# county_name = df[ga_county, df.loc[6]]
-# print(county_name)
-# print(ga_county)
+df = pd.read_csv('statepop.csv')
+ga = df[df['STNAME'] == 'Georgia']
+
+values = df['TOT_POP'].tolist()
+fips = df['FIPS'].tolist()
+
+fig = ff.create_choropleth(
+    fips=fips, values=values, scope=['GA']
+)
+
+fig.layout.template = None
+fig.show()
+# df.rename(columns={'Unnamed: 2': 'county_code', 'Unnamed: 1': 'state_code',
+#                 'Unnamed: 6': 'area_name'}, inplace=True)
+# ga_county = (df['state_code'] == '13') & (df['area_name'].str.contains('County')) & (df['county_code'] != '000')
+
+# print(df.columns)
+# # fips = df['FIPS'].tolist()
+#
+# ga_counties = df[df['state_code'] == '13']
+# remove_rows_0 = ()
+#
+# fips = ga_counties['fips'].tolist()
+# # print(df.loc[ga_county])
+# print(fips)
