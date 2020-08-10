f3859e3ac990773dd103bd5894953ecbfd5cf83e
kotternaj <jreddock@gmail.com>
2020-08-09 23:59:41 -0400

    created map

--- dash_bee.py
+++ dash_bee.py
@@ -13,7 +13,7 @@ df = pd.read_csv("intro_bees.csv")
 # print(df)
 df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
 df.reset_index(inplace=True)
-print(df[:5])
+# print(df[:5])
 
 app.layout = html.Div([
     html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
@@ -31,9 +31,36 @@ app.layout = html.Div([
 
     html.Div(id='output_container', children=[]),
     html.Br(),
-    dcc.Graph(id='bee_map', figure={})
+    dcc.Graph(id='my_bee_map', figure={})
 ])
 
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='my_bee_map', component_property='figure')],
+    [Input(component_id='slct_year', component_property='value')]
+)
+def update_graph(option_slctd):
+    print(option_slctd)
+    container = "The year chosen by user was: {}".format(option_slctd)
 
- # if __name__ == '__main__':
- #     app.run_server(debug=True)
+    dff = df.copy()
+    dff = dff[dff['Year'] == option_slctd]
+    dff = dff[dff['Affected by'] == 'Varroa_mites']
+
+    fig = px.choropleth(
+        data_frame=dff,
+        locationmode='USA-states',
+        locations='state_code',
+        scope="usa",
+        color='Pct of Colonies Impacted',
+        hover_data=['State', 'Pct of Colonies Impacted'],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+        template='plotly_dark'
+    )
+
+    return container, fig
+
+
+if __name__ == '__main__':
+     app.run_server(debug=True)
